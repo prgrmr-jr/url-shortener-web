908ce33b1beb0c3ba4516d005fdd01233a66983c
Jr <alfante.joserey03@gmail.com>
2025-01-04 02:16:29 +0800

    added a new loader for redirecting the page

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Page({
@@ -9,13 +9,28 @@ export default function Page({
   params: Promise<{ slug: string }>;
 }) {
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const { slug } = await params;
+      setIsLoading(true);
       router.push(`/api/shorten/${slug}`);
     })();
   }, [params, router]);
 
-  return <div>Redirecting...</div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      {isLoading ? (
+        <div className="text-center  max-w-md mx-auto">
+          <p className="text-xl font-semibold text-black mb-4 animate-pulse">
+            Please wait, redirecting...
+          </p>
+          <p className="text-sm text-gray-600">This may take a few seconds.</p>
+        </div>
+      ) : (
+        <p className="text-black">Redirecting...</p>
+      )}
+    </div>
+  );
 }
